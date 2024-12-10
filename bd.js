cde24fa8aaa0c098a7dfe8c094e282ccec5d455a
VasKito43 <130059430+VasKito43@users.noreply.github.com>
2024-12-10 17:39:09 -0300

    10/12

--- bd.js
+++ bd.js
@@ -1,9 +1,7 @@
-import 'dotenv/config';
-import postgres from 'postgres';
+import 'dotenv/config'
+import postgres from 'postgres'
 
-export const sql = postgres(
-  process.env.URL,
-  {
-    ssl: { rejectUnauthorized: false }, // Configuração correta para SSL
-  }
-);
+export const sql = postgres (
+    process.env.URL,
+    { ssl: 'require' }
+)
